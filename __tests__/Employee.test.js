a88a7aa1b8e15c0c68b047f77ae3265c857260e7
Plain Jane <jane99hsu@gmail.com>
2020-10-09 16:42:21 -0700

    added Employee constructor tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@jest/globals');
+// ============================= imports go here ============================== //
+const { TestScheduler } = require('jest');
+
+const Employee = require('../lib/Employee.js');
+// ============================= imports go here ============================== //
+
+test('creates an employee object', () => {
+    const employee = new Employee('Jane Hsu', 12345, "jane@gmail.com");
+
+    expect(employee.name).toMatch(/ /);
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toMatch(/@/ && /./);
+});
