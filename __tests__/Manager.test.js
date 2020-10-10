18d1aa8ff01ccfd52b5a59609ddd326da124b31c
Plain Jane <jane99hsu@gmail.com>
2020-10-09 20:32:57 -0700

    added Manager prompts

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,11 @@
+// ============================= imports go here ============================== //
+const { TestScheduler } = require('jest');
+
+const Manager = require('../lib/Manager.js');
+// ============================= imports go here ============================== //
+
+test('creates a Manager object', () => {
+    const manager = new Manager('Jane Hsu', 12345, "jane@gmail.com", "321-456-7890");
+
+    expect(manager.officeNumber).toEqual(expect.any(String));
+});
--- index.js
+++ index.js
@@ -2,26 +2,63 @@
 const inquirer = require('inquirer');
 // ============================= imports go here ============================== //
 
-
-inquirer
-    .prompt([
+// function to prompt the user for manager data
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'manager-name',
-            message: "Please input Manager's name."
+            message: "Please enter Manager's name.",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter Manager's name.");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'manager-id',
-            message: "Please input Manager's ID#."
+            message: "Please enter Manager's ID#.",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log("Please enter an ID#.");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'manager-email',
-            message: "Please input Manager's email."
+            message: "Please enter Manager's email.",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter an email.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'manager-phone',
+            message: "Please enter Manager's phone number XXX-XXX-XXXX.",
+            validate: phoneInput => {
+                if (phoneInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a phone number.");
+                    return false;
+                }
+            }
         }
-    ])
-    // .then(({ name }) => {
-    //     this.manager = new Manager(name);
-    // })
-;
+    ]);
+};
+
+// function to start the application
+promptUser();
