d32255518441c53ffe78f4d8746394d0fbc3466e
Plain Jane <jane99hsu@gmail.com>
2020-10-09 22:07:55 -0700

    started basic html & css to add Manager data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // ============================= imports go here ============================== //
-const inquirer = require('inquirer');
+const inquirer = require('inquirer'); // access the inquirer module //
+const generatePage = require('./src/page-template.js');
 // ============================= imports go here ============================== //
 
-// function to prompt the user for manager data
+// function that starts the application
+// prompts the user for manager data
 const promptUser = () => {
     return inquirer.prompt([
         {
@@ -60,5 +62,29 @@ const promptUser = () => {
     ]);
 };
 
+// function that prompts for team member data
+const promptTeam = teamData => {
+    return teamData;
+}
+
 // function to start the application
-promptUser();
+// promptUser outputs an object
+promptUser()
+    // recursively call promptTeam for as many team members as the user wants to add
+    // each team member will be pushed into a team member array in a data object
+    .then(promptTeam)
+
+    // pass team member data into generatePage function and return html code
+    .then(teamData => {
+        return generatePage(teamData);
+    })
+    
+    // pass html code to writeFile
+    .then(pageHTML => {
+        return msWriteProfilerMark(pageHTML);
+    })
+
+    .catch(err => {
+        console.log(err);
+    })
+;
---
+++ src/page-template.js
@@ -0,0 +1,19 @@
+const generatePage = (data) => {
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Team Profile</title>
+                <link rel="stylesheet" href="style.css">
+            </head>
+
+            <body>
+                <header>
+                
+            </body>
+        </html>
+        
+    `
+}
