b2fe9513cc47a67dcc636d9d3f51fc26295f3833
Plain Jane <jane99hsu@gmail.com>
2020-10-10 19:32:32 -0700

    added Engineer test with passing constructor

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,11 @@
+// ============================= imports go here ============================== //
+const { TestScheduler } = require('jest');
+
+const Engineer = require('../lib/Engineer.js');
+// ============================= imports go here ============================== //
+
+test('creates an Engineer object', () => {
+    const engineer = new Engineer('Jane Hsu', 12345, "jane@gmail.com", "plainjane99");
+
+    expect(engineer.gitHub).toEqual(expect.any(String));
+});
